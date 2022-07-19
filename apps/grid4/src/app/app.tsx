e2b75cdaefbe77cd49f36db6ad355d8c0e257989
goyal.vaibhav1610@gmail.com <goyal.vaibhav1610@gmail.com>
2022-07-19 20:30:29 +0530

    initial commit

---
+++ apps/grid4/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="grid4" />
+      <div />
+    </>
+  );
+}
+
+export default App;
